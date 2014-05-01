26f8c420a105dcaded2927a07759e843838e5ffb
Jonathan Häberle <jonathan.haeberle@gmail.com>
2014-05-01 17:42:05 +0200

    working two remote collections

--- lib/model4.ts
+++ lib/model4.ts
@@ -1,3 +1,4 @@
+
 var ObjectId = (function() {
     var cnt = -1;
     return function () {
@@ -5,31 +6,6 @@ var ObjectId = (function() {
         return "id:"+cnt;
     }
 })();
-
-class Transport {
-    static transport;
-
-    private collection : Collection;
-    constructor(collection : Collection) {
-        this.collection = collection;
-    }
-
-    sendUpdate(obj : Model) {
-        //console.log("sendUpdate()", obj.toJSON());
-    }
-
-    sendCreate(obj : Model) {
-        //console.log("sendCreate()", obj.toJSON());
-    }
-
-    receiveUpdate(newObject : Model) {
-        this.collection.update(newObject);
-    }
-
-    receiveCreate(newObject : Model) {
-        this.collection.create(newObject);
-    }
-}
 
 
 class View<T extends Model> {
@@ -44,6 +20,7 @@ class View<T extends Model> {
         this._map = map;
         this._collection.addView(this);
 
+        // definition of the emit function
         var self = this;
         this.emit = function(key:string, obj:T) {
             self._objs[key] = obj;
@@ -71,9 +48,75 @@ class View<T extends Model> {
     }
 }
 
+class LocalConnector {
+    private transport1;
+    private transport2;
+
+    constructor(trans1 : Transport, trans2 : Transport) {
+        this.transport1 = trans1;
+        this.transport2 = trans2;
+
+        // setup Transport 1
+        this.transport1.sendUpdate = (obj : Model) => {
+            var objJSON = obj.toJSON();
+            var revObj = <Model>obj;
+            this.transport2.receiveUpdate(revObj);
+        }
+
+        this.transport1.sendCreate = (obj : Model) => {
+            var objJSON = obj.toJSON();
+            var revObj = <Model>obj;
+
+            this.transport2.receiveCreate(revObj);
+        }
+
+        // setup Transport 2
+        this.transport2.sendUpdate = (obj : Model) => {
+            var objJSON = obj.toJSON();
+            var revObj = <Model>obj;
+
+            this.transport1.receiveUpdate(revObj);
+        }
+
+        this.transport2.sendCreate = (obj : Model) => {
+            var objJSON = obj.toJSON();
+            var revObj = <Model>obj;
+
+            this.transport1.receiveCreate(revObj);
+        }
+
+    }
+}
+
+class Transport {
+
+    private collection : Collection;
+    constructor(collection : Collection) {
+        this.collection = collection;
+    }
+
+    sendUpdate(obj : Model) {
+        //console.log("sendUpdate()", obj.toJSON());
+    }
+
+    sendCreate(obj : Model) {
+        //console.log("sendCreate()", obj.toJSON());
+    }
+
+    receiveUpdate(newObject : Model) {
+        this.collection.update(newObject);
+    }
+
+    receiveCreate(newObject : Model) {
+        this.collection.create(newObject);
+    }
+}
+
+
 class Collection {
     private objs : {[id:string] : Model} = {};
     private views : View<Model>[] = [];
+    private transport : Transport;
 
     getObject(id : string) : Model {
         return this.objs[id];
@@ -98,20 +141,24 @@ class Collection {
     save(obj : Model) {
         if (this.objs[obj.id]) {
             this.update(obj);
-            Transport.transport.sendUpdate(obj);
+            this.transport.sendUpdate(obj);
         } else {
             this.create(obj);
-            Transport.transport.sendCreate(obj);
+            this.transport.sendCreate(obj);
         }
     }
 
     addView(view : View<Model>) {
         this.views.push(view);
     }
+
+    setTransport(transport : Transport) {
+        this.transport = transport;
+    }
 }
 
-class Model {
 
+class Model {
 
     constructor(modelName : string) {
         this.type = modelName;
@@ -158,6 +205,7 @@ class Model {
     type : string;
 }
 
+// Example Models
 
 class User extends Model {
     constructor() { super("users"); }
@@ -168,7 +216,17 @@ class User extends Model {
 }
 
 var theCollection = new Collection();
-Transport.transport = new Transport(theCollection)
+var theTransport = new Transport(theCollection)
+theCollection.setTransport(theTransport);
+
+
+var remoteCollection = new Collection();
+var remoteTransport = new Transport(remoteCollection);
+remoteCollection.setTransport(remoteTransport);
+
+var theConnector = new LocalConnector(theTransport, remoteTransport);
+
+
 
 var fooView = new View<User>((obj, emit) => {
     if (obj.type === "users") {
@@ -187,24 +245,39 @@ fooView.bind( (objs) => {
 });
 
 
+
+var remoteView = new View<User>((obj, emit) => {
+    emit(user.id, user);
+}, remoteCollection);
+
+remoteView.bind( (objs) => {
+    console.log("binding remote");
+    for (var key in objs) {
+        console.log(key, " -> ", objs[key].toJSON());
+    }
+});
+
+
+
 var user = new User();
+user.setCollection(theCollection);
 user.name = "jonthan";
 user.adr = "foo";
-
-user.setCollection(theCollection);
-
 user.save();
 
 
 var user2 = new User();
+user2.setCollection(theCollection);
 user2.name = "matthias";
 user2.adr = "foo";
-user2.setCollection(theCollection);
 
-Transport.transport.receiveUpdate(user2);
+theTransport.receiveUpdate(user2);
 
 var user3 = new User();
+user3.setCollection(theCollection);
 user3.name = "jonathan";
 user3.adr = "bar";
-user3.setCollection(theCollection);
 user3.save();
+
+
+
